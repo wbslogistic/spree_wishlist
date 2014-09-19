fd0e8506094ccd06855db4f6dfd034cf884cb5e6
Oleg Krasnov <mail@olegkrasnov.com>
2014-09-19 18:53:11 +0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Spree::Core::Engine.add_routes do
   resources :wishlists
   resources :wished_products, only: [:create, :update, :destroy]
   get '/wishlist' => "wishlists#default", as: "default_wishlist"
+  get '/destroy_all' => "wished_products#destroy_all", as: "destroy_all"
 end
